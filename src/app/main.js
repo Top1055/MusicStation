93ae3f940bd07579f109c0b0531f4d7ab83e35e8
Alex <Alex@DESKTOP-V2K6MEE>
2021-06-25 03:39:30 +0100

    Basic modal complete, needs css & js

--- src/app/main.js
+++ src/app/main.js
@@ -1,34 +1,22 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("include-html");
-        if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /* Remove the attribute, and call this function once more: */
-                    elmnt.removeAttribute("include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /* Exit the function: */
-            return;
-        }
-    }
-}
-includeHTML()
 const createBtn = document.querySelector('#createpl');
 const modal = document.querySelector('.modal');
+const x = document.querySelector('.close');
 
-createBtn.addEventListener('click', () => {
-    modal.style.display = "block";
-});
+function toggleModal() {
+    if(modal.style.display == "block") {
+        modal.style.display = "none";
+        x.style.display = "none";
+    } else {
+        modal.style.display = "block";
+        x.style.display = "block";
+    }
+}
+
+x.addEventListener('click', toggleModal)
+createBtn.addEventListener('click', toggleModal)
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
